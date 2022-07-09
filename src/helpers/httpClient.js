86e25b4f53443092d1d754a2c4b47cf4787fe1a3
Nicolás Herrera <josnikh@gmail.com>
2022-07-08 19:17:17 -0500

    Creation of Axios HTTP Client

---
+++ src/helpers/httpClient.js
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+
+const API = axios.create({
+    baseURL: process.env.API_GATEWAY_URL,
+    timeout: 4000,
+    headers: {'accept': 'application/json'}
+})
+
+axios.interceptors.request.use(request => {
+
+    const authState = useSelector((state) => state);
+
+    if (authState.isAuthenticated && authState.token !== ""){
+        request.headers.common.Authorization = `Bearer ${authState.token}`;
+    }
+    return request;
+});
+
+const errorHandler = (error) => {
+    if (error.response){
+        return {
+            type: "response",
+            error: error.response
+        }
+    }else if (error.request){
+        return {
+            type: "request",
+            error: error.request
+        }
+    }else{
+        return {
+            type: "unknown",
+            error: error.message
+        }
+    }
+}
+
+export default {
+    post: async (path, payload) => {
+        try{
+            return await API.post(path, payload);
+        }catch(e){
+            return {
+                data: errorHandler(e),
+                error: true
+            }
+        }
+        
+    },
+    get: async (path) => {
+        try{
+            return await API.get(path);
+        }catch(e){
+            return {
+                data: errorHandler(e),
+                error: true
+            }
+        }
+    }
+}
