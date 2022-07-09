583dc2594bd4b1511cc5190e053643a3de77f917
Nicolás Herrera <josnikh@gmail.com>
2022-07-08 19:16:48 -0500

    implements protected screen

--- src/route/index.js
+++ src/route/index.js
@@ -1,11 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./../pages/Login";
+import Home from "./../pages/Home";
+
+import { useSelector } from 'react-redux';
+
+
+const ProtectedRoute = ({ authState, children }) => {
+    if (!authState.isAuthenticated ||  authState.token !== "") {
+        return <Navigate to="/" replace />;
+    }
+
+    return children;
+};
 
 const RouteApp = () =>{
+const authState = useSelector((state) => state);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />}>
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute authState={authState}>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
         </Route>
       </Routes>
     </BrowserRouter>
